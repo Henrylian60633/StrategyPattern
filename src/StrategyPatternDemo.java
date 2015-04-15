49dcc4edf50ff14475887f53433ac2953d5db36d
Henrylian60633 <h569696@gmail.com>
2015-04-15 15:50:58 +0800

    StrateryPattern

---
+++ src/StrategyPatternDemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 子恒
+ */
+public class StrategyPatternDemo {
+    public static void main(String[] args) {
+      Context context = new Context(new OperatinAdd());		
+      System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+      context = new Context(new OperationSubstract());		
+      System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+      context = new Context(new OperationMultiply());		
+      System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+   }
+}
